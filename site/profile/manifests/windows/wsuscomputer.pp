5637466bc9fb3cafac40a65c2c654eeae9067d39
TraGicCode <6224270+TraGicCode@users.noreply.github.com>
2017-10-24 07:45:48 -0500

    WIP

--- site/profile/manifests/windows/wsuscomputer.pp
+++ site/profile/manifests/windows/wsuscomputer.pp
@@ -3,7 +3,7 @@
 #
 class profile::windows::wsuscomputer {
     class { 'wsus_client':
-        server_url           => 'http://10.20.1.14:8530',
+        server_url           => 'http://10.20.1.15:8530',
         enable_status_server => true,
         target_group         => 'services',
     }
