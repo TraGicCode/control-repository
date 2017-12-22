096d7ea8fbeb6e8e386c855f355ce59b37af12ae
TraGicCode <6224270+TraGicCode@users.noreply.github.com>
2017-12-21 20:45:01 -0600

    WIP

--- site/profile/manifests/windows/sqlserverhadr/filesharewitness.pp
+++ site/profile/manifests/windows/sqlserverhadr/filesharewitness.pp
@@ -2,20 +2,20 @@
 #
 # NOTE: The FileShare must live on a node that is not in the cluster ( in my case i selected my dc )
 class profile::windows::sqlserverhadr::filesharewitness(
-  String $domain_administrator_user,
-  String $domain_administrator_password,
-  Pattern[/^\d+\.\d+\.\d+\.\d+$/] $fail_over_cluster_static_ip,
 ) {
 
-  redact('domain_administrator_password')
+  file { 'C:\FileShareWitness':
+    ensure => 'directory',
+  }
 
   # New-SmbShare -Name "FileShareWitness" -Path 'C:\FileShareWitness' -ChangeAccess @("tragiccode\supercluster$", "tragiccode\ad_principal_manager")
-  dsc_xsmbshare { 'FileShareForWitness':
-    dsc_ensure      => 'present' ,
-    dsc_name        => 'FileShareForWitness',
-    dsc_path        => 'C:\\WSFC-FileShare-Witness',
-    # dsc_readaccess => "User1"
-    # dsc_noaccess = @("User3", "User4")
-    dsc_description => 'Used for fail over clustering with sql server',
+  dsc_xsmbshare { 'FileShareWitness':
+    dsc_ensure       => 'present' ,
+    dsc_name         => 'FileShareWitness',
+    dsc_path         => 'C:\\WSFC-FileShare-Witness',
+    dsc_readaccess   => [ 'tragiccode\\ad_principal_manager' ],
+    dsc_changeaccess => [ 'tragiccode\\my-cluster' ],
+    dsc_description  => 'Used to break the tie in windows server failover clustering for SQL Server Always-On',
+    require          => File['C:\\FileShareWitness'],
   }
 }
