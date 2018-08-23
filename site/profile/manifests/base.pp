807f3b97fb085509f93402ebf6da789bbb2c4ffd
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-08-23 16:00:00 -0500

    Update baseline formatting

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,9 +4,15 @@ class profile::base {
   # profile::base must be assigned to every machine, including workstations. 
   # It manages basic policies, and uses some conditional logic to include OS-specific profiles as needed.
   case $facts['kernel'] {
-    'Linux':   { include profile::linux::base   }
-    'windows': { include profile::windows::base }
-    default:   { }
+    'Linux': {
+      include profile::linux::base
+    }
+    'windows': {
+      include profile::windows::base
+    }
+    default: {
+      fail('Unsupported operating system!')
+    }
   }
 
 }
