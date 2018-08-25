5550db167a8162b5972b6ee7aa3bcd3224e400da
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-08-25 06:08:36 -0500

    Lets test the jenkinsmaster docker container

--- spec/classes/role/jenkinsmaster_spec.rb
+++ spec/classes/role/jenkinsmaster_spec.rb
@@ -1,19 +1,20 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'role::jenkinsmaster' do
-#   context 'should compile into a catalogue without dependency cycles' do
-#     let(:facts) do
-#       {
-#         osfamily: 'Debian',
-#         operatingsystemrelease: '16.04',
-#         architecture: 'amd64',
-#         lsbdistcodename: 'xenial',
-#         os: { 'name' => 'Ubuntu', 'release' => { 'full' => '16.04' } },
-#         jenkins_plugins: nil,
-#         kernel: 'Linux',
-#       }
-#     end
+describe 'role::jenkinsmaster' do
+  context 'should compile into a catalogue without dependency cycles' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystemrelease: '16.04',
+        operatingsystem: 'Ubuntu',
+        architecture: 'amd64',
+        lsbdistcodename: 'xenial',
+        os: { 'name' => 'Ubuntu', 'release' => { 'full' => '16.04' } },
+        jenkins_plugins: nil,
+        kernel: 'Linux',
+      }
+    end
 
-#     it { is_expected.to compile }
-#   end
-# end
+    it { is_expected.to compile }
+  end
+end
