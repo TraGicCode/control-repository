c5a192cd706ee416d525be8c6a47b4cf6a177f4f
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-08-30 01:31:55 -0500

    Add comments about master requirements

--- site/profile/manifests/linux/jenkins/slave.pp
+++ site/profile/manifests/linux/jenkins/slave.pp
@@ -2,7 +2,10 @@ class profile::linux::jenkins::slave(
   String[1] $autojoiner_username,
   Sensitive[String[1]] $sensitive_autojoiner_password,
 ) {
-
+  # requires the following on the master
+  # 1 . Self-Organizing Swarm Plug-in Modules be installed
+  # 2. Enable protocol:Protocol:JNLP2-connect
+  #     Manage Jenkins > Configure Global Security
   class { 'jenkins::slave':
       masterurl                => 'http://jenkinsmaster-001.local:8080',
       ui_user                  => $autojoiner_username,
