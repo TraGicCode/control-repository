f3dd3a435be7e7b62b9437d591c8181ceffd97ab
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-08-30 01:38:34 -0500

    Add ruby to slave

--- site/role/manifests/jenkinsslave.pp
+++ site/role/manifests/jenkinsslave.pp
@@ -2,11 +2,13 @@ class role::jenkinsslave {
 
     include profile::base
     include profile::linux::git
+    include profile::linux::continuousintegration::ruby
     include profile::linux::jenkins::slave
     include profile::linux::splunk::universalforwarder
 
     Class['profile::base']
     -> Class['profile::linux::git']
+    -> Class['profile::linux::continuousintegration::ruby']
     -> Class['profile::linux::jenkins::slave']
     -> Class['profile::linux::splunk::universalforwarder']
 }
