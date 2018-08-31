33461a33836f7e98adc302762b4d336152af4653
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-08-30 19:24:04 -0500

    Added puppetmaster host entry to jenkins docker container

--- site/profile/manifests/linux/jenkinsmaster.pp
+++ site/profile/manifests/linux/jenkinsmaster.pp
@@ -13,6 +13,9 @@ class profile::linux::jenkinsmaster {
   docker::run { 'jenkins_demo':
     image            => 'jenkinsci/blueocean:latest',
     ports            => ['8080:8080','50000:50000'],
+    hostentries      => ['puppetmaster-001.local:10.20.1.2'],
+    # This allows us to manage it with puppet without having to blow away the container
+    # on every change to this resource
     extra_parameters => [ '--restart=always' ],
   }
 }
