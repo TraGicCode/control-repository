8f56488cf08489643f64868ebbd4465f6738857f
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-09-03 00:33:10 -0500

    WIP

--- site/role/manifests/test.pp
+++ site/role/manifests/test.pp
@@ -1,22 +1,10 @@
 class role::test {
-  include profile::base
 
-  package { 'awscli':
-    ensure   => 'present',
-    provider => 'chocolatey',
-  }
 
   notify { 'test':
 
   }
 
-  refresh_environment { 'my-stuff':
-    subscribe => Notify['test'],
-  }
 
-  archive { 'C:\\americanblinds-logo.png':
-    ensure => present,
-    source => 's3://americanblindsmarketingimages/images/americanblinds-logo.png',
-  }
 
 }
