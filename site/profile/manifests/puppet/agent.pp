1d5529bb6882832527747c536cd811b61b2573ef
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-09-10 05:05:14 -0500

    Update squid module

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -1,5 +1,7 @@
 class profile::puppet::agent {
 
+  # https://github.com/LMacchi/puppet_agent_settings allows us to get agent puppet.conf settings
+  # Add it later
   $puppet_conf = $facts['kernel'] ? {
     'windows' => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
     default   => '/etc/puppetlabs/puppet/puppet.conf',
@@ -8,6 +10,7 @@ class profile::puppet::agent {
   ini_setting { 'use_cached_catalog':
     ensure  => present,
     path    => $puppet_conf,
+    # path    => $facts['puppet_agent_settings']['config'],
     section => 'agent',
     setting => 'use_cached_catalog',
     # lint:ignore:quoted_booleans
