aac8720e5877bc3cd2da0cf134ed19b7234b95f2
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-09-10 00:37:35 -0500

    wip

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -1,7 +1,13 @@
 class profile::puppet::agent {
+
+  $puppet_conf = $facts['kernel'] ? {
+    'windows' => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
+    default   => '/etc/puppetlabs/puppet/puppet.conf',
+  }
+
   ini_setting { 'use_cached_catalog':
     ensure  => present,
-    path    => $::settings::config,
+    path    => $puppet_conf,
     section => 'agent',
     setting => 'use_cached_catalog',
     # lint:ignore:quoted_booleans
