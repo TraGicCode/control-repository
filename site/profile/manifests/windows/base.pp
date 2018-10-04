a841fc6c95168c095116505a98bf7ebfacdc7111
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-10-04 14:24:00 -0500

    WIP

--- site/profile/manifests/windows/base.pp
+++ site/profile/manifests/windows/base.pp
@@ -24,6 +24,83 @@ class profile::windows::base {
     log_output         => true,
   }
 
+  chocolateyconfig { 'cacheLocation':
+    ensure => 'present',
+    value  => 'C:\\ProgramData\\choco-cache',
+  }
+
+  chocolateyconfig { 'commandExecutionTimeoutSeconds':
+    ensure => 'present',
+    value  => '2700',
+  }
+
+  chocolateyfeature { 'allowemptychecksums':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'allowemptychecksumssecure':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'allowglobalconfirmation':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'autouninstaller':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'checksumfiles':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'failonautouninstaller':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'failoninvalidormissinglicense':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'failonstandarderror':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'ignoreinvalidoptionsswitches':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'ignoreunfoundpackagesonupgradeoutdated':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'logenvironmentvalues':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'logwithoutcolor':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'powershellhost':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'removepackageinformationonuninstall':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'scriptschecklastexitcode':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'showdownloadprogress':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'shownonelevatedwarnings':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'stoponfirstpackagefailure':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'usefipscompliantchecksums':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'usepackageexitcodes':
+    ensure => 'enabled',
+  }
+  chocolateyfeature { 'userememberedargumentsforupgrades':
+    ensure => 'disabled',
+  }
+  chocolateyfeature { 'viruscheck':
+    ensure => 'disabled',
+  }
+
   dsc_xtimezone { 'Server Timezone':
     ensure               => 'present',
     dsc_issingleinstance => 'yes',
