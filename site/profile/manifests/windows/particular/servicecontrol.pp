bd05e98dddc3a8f0270b31847caf1ab2afd7ac09
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-10-04 14:50:35 -0500

    GO

--- site/profile/manifests/windows/particular/servicecontrol.pp
+++ site/profile/manifests/windows/particular/servicecontrol.pp
@@ -3,13 +3,10 @@
 #
 class profile::windows::particular::servicecontrol {
   class { 'nservicebusservicecontrol':
-      package_ensure                     => 'present',
-      file_source                        => 'https://storage.googleapis.com/puppet-provisioning-binaries/NServiceBus%20ServiceControl/Latest/Particular.ServiceControl-1.41.3.exe',
-      file_path                          => 'C:\\Particular.ServiceControl-1.41.3.exe',
-      package_name                       => 'Particular Software ServiceControl Management',
-      package_install_options_log_path   => 'C:\\Particular.ServiceControl.install.log',
-      package_uninstall_options_log_path => 'C:\\Particular.ServiceControl.uninstall.log',
-      # license_xml                        => Sensitive(''),
+      package_ensure     => 'present',
+      remote_file_source => 'https://storage.googleapis.com/puppet-provisioning-binaries/NServiceBus%20ServiceControl/Latest/Particular.ServiceControl-1.41.3.exe',
+      remote_file_path   => 'C:\\Particular.ServiceControl-1.41.3.exe',
+      # license_xml                      => Sensitive(''),
   }
 
   nservicebusservicecontrol::instance { 'Particular.ServiceControl.Development':
